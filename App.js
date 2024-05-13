c591af361d958ad9ccc229ccecd33bc52e241517
Samil Abud <samilabud@gmail.com>
2024-05-12 22:10:53 -0400

    Updating react native / expo  to 2024 version

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+import { StyleSheet, SafeAreaView, Platform } from "react-native";
+import colors from "./src/utils/colors";
+import Focus from "./src/features/focus";
+import { FocusHistory } from "./src/features/FocusHistory";
+import { Timer } from "./src/features/timer";
 
 export default function App() {
+  const [currentSubject, setCurrentSubject] = React.useState(null);
+  const [history, setHistory] = React.useState([]);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {currentSubject ? (
+        <Timer
+          focusSubject={currentSubject}
+          onTimerEnd={(subject) => {
+            setHistory([...history, subject]);
+          }}
+          clearSubject={() => setCurrentSubject(null)}
+        />
+      ) : (
+        <>
+          <Focus addSubject={setCurrentSubject} />
+          <FocusHistory history={history} />
+        </>
+      )}
+    </SafeAreaView>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 10,
+    backgroundColor: colors.darkBlue,
   },
 });
